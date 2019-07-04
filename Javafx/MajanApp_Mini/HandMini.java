29d718ed76dcf9a30013741822a24d00c1271988
nya <ss.physigra@gmail.com>
2019-07-04 16:56:10 +0900

    “change_MajanAppMini”

--- Javafx/MajanApp_Mini/HandMini.java
+++ Javafx/MajanApp_Mini/HandMini.java
@@ -13,6 +13,14 @@ interface InterHandMini{
 }
 
 class HandMini implements InterHandMini{
+    private final String[][] syurui ={
+        {"m1","m2","m3","m4","m5","m6","m7","m8","m9"},
+        {"s1","s2","s3","s4","s5","s6","s7","s8","s9"},
+        {"p1","p2","p3","p4","p5","p6","p7","p8","p9"},
+        {"東","南","西","北","白","發","中"}};
+
+    private final String[] Op = {"ポン","チー","ロン","ツモ"};
+    private final String[] Ent = {"戻る","決定"};
     private String[][] Men = {{"?","?","?"},
                               {"?","?","?"},
                               {"?","?","?"},
@@ -42,6 +50,18 @@ class HandMini implements InterHandMini{
     }
     public void setYakuhai(String s){
         Yakuhai = s;
+    }
+
+    public String getSyurui(int i, int j){
+        return syurui[i][j];
+    }
+
+    public String getOp(int i){
+        return Op[i];
+    }
+
+    public String getEnt(int i){
+        return Ent[i];
     }
 
     public String getMen(int i,int j){
--- Javafx/MajanApp_Mini/MajanMini.java
+++ Javafx/MajanApp_Mini/MajanMini.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.geometry.*;
 
 public class MajanMini extends Application{
+    HandMini hand = new HandMini();
     private Button[][] bt = new Button[4][9];
     private Button[] bt2 = new Button[4];
     private Button[] bt3 = new Button[2];
@@ -25,26 +26,21 @@ public class MajanMini extends Application{
         stage.setWidth(800);
         stage.setHeight(400);
 
-        String[][] syurui ={
-            {"m1","m2","m3","m4","m5","m6","m7","m8","m9"},
-            {"s1","s2","s3","s4","s5","s6","s7","s8","s9"},
-            {"p1","p2","p3","p4","p5","p6","p7","p8","p9"},
-            {"東","南","西","北","白","發","中"}};
-
-        String[] Op = {"ポン","チー","ロン","ツモ"};
-        String[] Ent = {"戻る","決定"};
-
-        //ハイのボタンの作成・テキストをセット(中央)
+        //牌のボタンの作成・テキストをセット(中央)
         for(int i = 0; i < bt.length; i++){
             if(i == 3){
                 for(int j = 0; j < 7; j++){
-                    bt[i][j] = new Button(syurui[i][j]);
+                    bt[i][j] = new Button(hand.getSyurui(i,j));
                     bt[i][j].setPrefWidth(50);
+                    bt[i][j].setId(String.valueOf(i) + "," + String.valueOf(j));
+                    bt[i][j].setOnAction(new SampleEventHandler());
                }
             } else{
                 for(int j = 0; j < 9; j++){
-                    bt[i][j] = new Button(syurui[i][j]);
+                    bt[i][j] = new Button(hand.getSyurui(i,j));
                     bt[i][j].setPrefWidth(50);
+                    bt[i][j].setId(String.valueOf(i) + "," + String.valueOf(j));
+                    bt[i][j].setOnAction(new SampleEventHandler());
                }
             } 
         }
@@ -68,7 +64,7 @@ public class MajanMini extends Application{
         
         //オプションボタンを作成・テキストをセット(右側)
         for(int i = 0; i < bt2.length; i++){
-            bt2[i] = new Button(Op[i]);
+            bt2[i] = new Button(hand.getOp(i));
         }
 
         VBox vb = new VBox(10d);
@@ -79,7 +75,7 @@ public class MajanMini extends Application{
 
         //決定・戻るボタンの作成(下側)
         for(int i = 0; i < bt3.length; i++){
-            bt3[i] = new Button(Ent[i]);
+            bt3[i] = new Button(hand.getEnt(i));
         }
         
         HBox hb = new HBox(30d);
@@ -89,10 +85,10 @@ public class MajanMini extends Application{
             hb.getChildren().addAll(bt3[i]);
         }
 
-        //メンツの表示(左) <- Handからのゲッターで行う
+        //メンツの表示(左) 
         for(int i = 0; i < 5; i++){
             
-            for(int j = 0; j < 3; j++){     //getterで取得するようにする
+            for(int j = 0; j < 3; j++){     
                 String a = hand.getMen(i,j);
                 mentlabel[i][j] = new Label(a);
                 mentlabel[i][j].setPrefWidth(20);
@@ -124,48 +120,43 @@ public class MajanMini extends Application{
 
         //決定が押された後に頭・役牌・待ちの選択
 
-        //ここまで初期画面
-
-        /*
-        bt[0][0].setOnAction((ActionEvent e) -> {
-            hand.setMen(syurui[0][0],0,0);
-            mentlabel[0][0].setText(hand.getMen(0,0));
-        });
-        */
-
-        
-        bt[0][1].setOnAction((ActionEvent e) -> {  //？のところを見つけて入れる
-            for(int i = 0; i < 5; i++){
-                for(int j = 0; j < 3; j++){
-                    if(hand.getMen(i,j).equals("?")){
-                        hand.setMen(syurui[0][1],i,j);
-                        mentlabel[i][j].setText(hand.getMen(i,j));
-                    }
-                    break;
-                }
-                break;
-            }
-        });
-
-        bt[0][2].setOnAction((ActionEvent e) -> {  //？のところを見つけて入れる
-            for(int i = 0; i < 5; i++){
-                for(int j = 0; j < 3; j++){
-                    if(hand.getMen(i,j).equals("?")){
-                        hand.setMen(syurui[0][2],i,j);
-                        mentlabel[i][j].setText(syurui[i][j]);
-                    }
-                    break;
-                }
-                break;
-            }
-        });
-
-        
-
-
         stage.setScene(new Scene(root));
         stage.setTitle("符計算");
         stage.show();
 
     }
+
+    //牌が押された時の処理
+    class SampleEventHandler implements EventHandler<ActionEvent>{
+        public void handle(ActionEvent e){
+            Button b = (Button)e.getSource();
+            String Id = b.getId();
+            String[] Stid = Id.split(",", 0);
+            int x = Integer.parseInt(Stid[0]);
+            int y = Integer.parseInt(Stid[1]);
+            //lb.setText(((Button)e.getSource()).getId());
+
+            boolean flag = false;
+            for(int i = 0; i < 5; i++){
+                if(flag == true){
+                    break;
+                }
+                for(int j = 0; j < 3; j++){
+                    if(flag == true){
+                        break;
+                    }
+                    if(hand.getMen(i,j).equals("?")){
+                        hand.setMen(hand.getSyurui(x,y),i,j);
+                        mentlabel[i][j].setText(hand.getMen(i,j));
+                        flag = true;
+                    }
+                }
+            }
+
+            //hand.setMen(hand.getSyurui(x, y),x,y);
+            //mentlabel[x][y].setText(hand.getMen(x,y));
+        }
+    }
+    
 }
+
